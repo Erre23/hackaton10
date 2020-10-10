0445be0aa095a77057dafe5d8be542a003507711
Eduardo Rodriguez <lalorodrigez538@gmail.com>
2020-10-10 13:51:08 -0500

    Reto 2.20

--- js/Reto220.js
+++ js/Reto220.js
@@ -1,17 +1,17 @@
-String.vreplace(vocal) {
-    
-    this = this.replace("a", vocal);
-    this = this.replace("A", vocal);
-    this = this.replace("e", vocal);
-    this = this.replace("E", vocal);
-    this = this.replace("i", vocal);
-    this = this.replace("I", vocal);
-    this = this.replace("o", vocal);
-    this = this.replace("O", vocal);
-    this = this.replace("u", vocal);
-    this = this.replace("U", vocal);
+String.prototype.vreplace = function (vocal) {
+    let texto = this;
+    texto = texto.replace("a", vocal);
+    texto = texto.replace("A", vocal);
+    texto = texto.replace("e", vocal);
+    texto = texto.replace("E", vocal);
+    texto = texto.replace("i", vocal);
+    texto = texto.replace("I", vocal);
+    texto = texto.replace("o", vocal);
+    texto = texto.replace("O", vocal);
+    texto = texto.replace("u", vocal);
+    texto = texto.replace("U", vocal);
 
-    return this;
+    return texto;
 }
 
-console.log("apples and bananas".vreplace("a"));
+console.log("apples and bananas".vreplace("u"));
